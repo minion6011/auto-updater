6f160046f5a0ac324fcd599e441313bea1e83e3e
Minion6011 <mvittorio29@gmail.com>
2025-03-17 16:11:35 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -16,7 +16,7 @@ api_key = config["github_api_token"]
 files_to_update = config["files_to_update"]
 url = config["github_reposity_url"]
 
-bin_ext = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp', '.ico')
+bin_ext = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp', '.ico', ".ttf")
 
 # - Code
 
